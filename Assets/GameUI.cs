325be0a2d10e84bad77ba854943d8a50a84ed02d
Memoh <CraftMan3512@gmail.com>
2022-02-26 07:40:49 -0500

    start ui functionnality

---
+++ Assets/GameUI.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameUI : MonoBehaviour
+{
+
+    public TextMeshProUGUI gold, atk, health, days;
+    public KingdomsUISpawner kingdomUIs;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateUIValues();
+    }
+
+    public void UpdateUIValues()
+    {
+        
+        //GameManager.playerKingdom.
+        
+    }
+    
+}
