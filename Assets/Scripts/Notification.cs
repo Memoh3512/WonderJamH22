bfc3447cf9c35865be569069d96f3a956acfa334
Mattys Gervais <mattysgervais@gmail.com>
2022-02-26 14:39:39 -0500

    Notification start

---
+++ Assets/Scripts/Notification.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Notification
+{
+    public Sprite logo;
+    public string text;
+    public Notification(Sprite logo,string text)
+    {
+        this.logo = logo;
+        this.text = text;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
