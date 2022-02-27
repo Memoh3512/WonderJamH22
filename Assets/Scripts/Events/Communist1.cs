185b93d04348fef410f50e25ebbfe1729061d986
Mattys Gervais <mattysgervais@gmail.com>
2022-02-26 20:26:50 -0500

    Cost fix

--- Assets/Scripts/Events/Communist1.cs
+++ Assets/Scripts/Events/Communist1.cs
@@ -12,7 +12,7 @@ public class Communist1 : CardEvent
         Description = "A group of peasants would like to buy the abandoned church, as they just have founded a new religion!  " +
         "They have found a special connection with potatoes! They seem to whisper good cooking recipes. ";
 
-        getChoices.Add(new Choice(0,100,0,"Let them have the church",()=> {
+        getChoices.Add(new Choice(-20,0,0,"Let them have the church",()=> {
             GameManager.addPlannedEvent(new Communist21());
             return false;
         }));
