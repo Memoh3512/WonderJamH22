0040cd311bfea87ca278fff014144798996d68ee
Mattys Gervais <mattysgervais@gmail.com>
2022-02-26 22:12:48 -0500

    Shit unit

--- Assets/Scripts/poo/Unit.cs
+++ Assets/Scripts/poo/Unit.cs
@@ -10,17 +10,12 @@ public class Unit
     private float scale;
     private int mpValue;
 
-    public Unit(Sprite sprite, int hp, int damage, float scale) 
+    public Unit(Sprite sprite, int hp, int damage, int scale,int mpValue = 1)
     {
         this.Sprite = sprite;
         this.Hp = hp;
         this.Damage = damage;
         this.Scale = scale;
-        this.mpValue = 1;
-    }
-
-    public Unit(Sprite sprite, int hp, int damage, int scale,int mpValue = 1) : this(sprite,hp,damage,scale)
-    {
         this.mpValue = mpValue;
     }
 
