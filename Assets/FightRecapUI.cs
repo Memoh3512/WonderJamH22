2ba19877eee7cca6bf9f0b44599d474c36210081
Memoh <CraftMan3512@gmail.com>
2022-02-27 11:47:09 -0500

    stuff

--- Assets/FightRecapUI.cs
+++ Assets/FightRecapUI.cs
@@ -7,8 +7,8 @@ public class FightRecapUI : MonoBehaviour
 {
     private Canvas c;
 
-    public TextMeshProUGUI enemyStats;
     public TextMeshProUGUI kingdomStats;
+    public TextMeshProUGUI title;
 
     // Start is called before the first frame update
     void Start()
@@ -26,16 +26,12 @@ public class FightRecapUI : MonoBehaviour
 
     public void OpenMenu(string whowon,int loseAll,int loseEn)
     {
+        var won = whowon == "Allies" ? "Victory!" : "Defeat...";
 
-        string won = whowon == "Enemies" ? "Won!" : "Lost!";
-
-        string enemies = FightManager.woundedEnemies.Count == 0 ? "no" : FightManager.woundedEnemies.Count.ToString();
-        enemyStats.text = $"ENEMY \n\n Lost {enemies} soldiers. \n\n {won} ";
-         
-        won = whowon == "Allies" ? "Won!" : "Lost!";
+        title.text = won;
         
         string player = FightManager.woundedAllies.Count == 0 ? "no" : (FightManager.woundedAllies.Count+FightManager.fullDeadAllies.Count).ToString();
-        kingdomStats.text = $"OUR KINGDOM \n\n Lost {player} soldiers. \n\n {won} \n\n - {loseAll} military power";
+        kingdomStats.text = $"We lost {loseAll} military power";
 
         c.enabled = true;
     }
--- Assets/Scripts/Events/Gardener.cs
+++ Assets/Scripts/Events/Gardener.cs
@@ -11,13 +11,13 @@ public class Gardener : CardEvent
         Description = "Hi! I'm a gardener and I would like to open a public garden. Can you lend me some money? ";
 
         getChoices.Add(new Choice(0,0,0,"Yes",()=> {
-            GameManager.AddEventForToday(new Message("YES","The garden is beautiful, people are happy, and some neighbors come to see this. It costs a little bit, but you make even more because people pay to see it","Nice"));
+            GameManager.AddEventForToday(new Message("Gardening is fun!","The garden is beautiful, people are happy, and some neighbors come to see this. It costs a little bit, but you make even more because people pay to see it.","Nice"));
             GameManager.playerKingdom.removeGold(-30);
             return false;
         }));
 
         getChoices.Add(new Choice(50,3,0,"No",()=> {
-            GameManager.AddEventForToday(new Message("NO","The gardener is so mad that he'll make poisonous plants grow everywhere in the village.","Noooooooo"));
+            GameManager.AddEventForToday(new Message("No","The gardener is so mad that he'll make poisonous plants grow everywhere in the village.","Noooooooo"));
             GameManager.playerKingdom.removeKingdomLife(15);
             return false;
         }));
