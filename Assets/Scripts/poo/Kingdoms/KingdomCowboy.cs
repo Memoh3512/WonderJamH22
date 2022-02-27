18e8353fd6c6690cec59e6c1ba8225fd69f439da
Antoine T-S <anto291100@gmail.com>
2022-02-26 20:23:37 -0500

    units sprites + base units

--- Assets/Scripts/poo/Kingdoms/KingdomCowboy.cs
+++ Assets/Scripts/poo/Kingdoms/KingdomCowboy.cs
@@ -7,19 +7,10 @@ public class KingdomCowboy : Kingdom
     public KingdomCowboy()
     {
         Name = "Cowboy";
+        BaseUnit = new Unit(Resources.Load<Sprite>("Units/cowboy"), 10, 5, 1);
         greediness = 100; // Largeur de la bande
         growth = 70; //50 = moyen + meilleur rendement
         variance = 2.4f; //1.7-2.5
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
