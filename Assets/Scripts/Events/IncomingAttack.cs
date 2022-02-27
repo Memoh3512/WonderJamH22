d416f6b2d95edf22a847ffd111e418a4344a0004
Memoh <CraftMan3512@gmail.com>
2022-02-27 12:08:25 -0500

    couple of fixes

--- Assets/Scripts/Events/IncomingAttack.cs
+++ Assets/Scripts/Events/IncomingAttack.cs
@@ -21,7 +21,17 @@ public class IncomingAttack : CardEvent
 
         this.Description = "You have received a message from your army commander : \n" +
                            "\"Your excellency, the " + attacker.Name + " Kingdom is at our doors! We must defend our land or find a way to make peace, quick!";
+       
+        Object.FindObjectOfType<Parallax>().TransitionTo(new List<Sprite>()
+        {
 
+            Resources.Load<Sprite>("Parallax/fire1"),
+            Resources.Load<Sprite>("Parallax/fire2"),
+            Resources.Load<Sprite>("Parallax/fire3")
+
+        });
+        
+        
         Choice choice1 = new Choice(0, 0, 0, "ALATAK!", () => {
             GameManager.fightOpponent = attacker;
             
@@ -35,6 +45,14 @@ public class IncomingAttack : CardEvent
             {
                 
                 GameManager.AddEventForToday(new Message("Peace at a cost", "You give a hefty sum of money to the " + attacker.Name + " Kingdom, and they decide to leave you alone... for now.", "That was a close one."));
+                Object.FindObjectOfType<Parallax>().TransitionTo(new List<Sprite>()
+                {
+
+                    Resources.Load<Sprite>("Parallax/Day1"),
+                    Resources.Load<Sprite>("Parallax/Day2"),
+                    Resources.Load<Sprite>("Parallax/Day3")
+
+                });
                 return false;
             }
             else
