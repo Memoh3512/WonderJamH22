b96de3bcd08c4cae9771c9c07a3e78ec094aa037
Memoh <CraftMan3512@gmail.com>
2022-02-26 20:50:03 -0500

    todo de plus florence!

---
+++ Assets/Swords.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swords : MonoBehaviour
+{
+   public void PlaySFX()
+   {
+      
+      //TODO FLO PLAY SON EPEES LONG DE TRNASITOTIOI
+      
+   }
+}
