6cc5b445930d0ac55ccd53b259e39f7f62e4cc0b
Memoh <CraftMan3512@gmail.com>
2022-02-26 22:48:09 -0500

    aadad

--- Assets/Scripts/Battle/FightManager.cs
+++ Assets/Scripts/Battle/FightManager.cs
@@ -31,7 +31,7 @@ public class FightManager : MonoBehaviour
 
         //DEBUG this
         //TODO remove icitte quand on a les kingdoms
-        if (!GameManager.firstPlay)
+        if (GameManager.firstPlay)
         {
             GameManager.fightOpponent = new KingdomCowboy();
             GameManager.playerKingdom = new Kingdom(true);
